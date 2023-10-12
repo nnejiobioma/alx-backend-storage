16faf6a8dd3528db0d728f498f81c55cb773a28f
obiomanneji <nnejiobioma@gmail.com>
2023-10-12 12:10:14 +0100

    task one added

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -7,3 +7,10 @@ IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'users
         name VARCHAR(255)
     );
 END IF;
+
+-- Creates table users with variables id, email and name
+--CREATE TABLE IF NOT EXISTS `users` (
+    --`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+   -- `email` VARCHAR(255) NOT NULL UNIQUE,
+   -- `name` VARCHAR(255)
+--);
---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+ -- Check if the 'users' table already exists
+CREATE TABLE IF NOT EXISTS `users` (  
+    -- If it doesn't exist, create the 'users' table
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255),
+    `country` ENUM('US', 'CO', 'TN') NOT NULL 
+)
