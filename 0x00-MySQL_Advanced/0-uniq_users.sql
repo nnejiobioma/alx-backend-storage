ce8a1341e65b324a9297f4bbb1fb51a27e38a31c
obiomanneji <nnejiobioma@gmail.com>
2023-10-12 12:12:51 +0100

    task zero handled

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,16 +1,7 @@
 -- Check if the 'users' table already exists
-IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'users') THEN
-    -- If it doesn't exist, create the 'users' table
-    CREATE TABLE users (
-        id SERIAL PRIMARY KEY,
-        email VARCHAR(255) NOT NULL UNIQUE,
-        name VARCHAR(255)
-    );
-END IF;
-
--- Creates table users with variables id, email and name
---CREATE TABLE IF NOT EXISTS `users` (
-    --`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-   -- `email` VARCHAR(255) NOT NULL UNIQUE,
-   -- `name` VARCHAR(255)
---);
+CREATE TABLE IF NOT EXISTS `users` (
+    --If it doesn't exist, create the 'users' table
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+   `email` VARCHAR(255) NOT NULL UNIQUE,
+   `name` VARCHAR(255)
+);
